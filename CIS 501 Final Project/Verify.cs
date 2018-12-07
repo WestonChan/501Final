f315808ba924a181d624dcc31ab817a877e2b234
wwchan <wwchan@OBAMA2>
2018-12-06 20:58:53 -0600

    Little bit of Verify

--- CIS 501 Final Project/Verify.cs
+++ CIS 501 Final Project/Verify.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections;
 namespace CIS_501_Final_Project
 {
     class Verify
     {
-        public Verify (Semester local, Semester ksis, out string output)
+        Semester local;
+        Semester ksis;
+        public Verify(Semester local, Semester ksis)
         {
-            string tempOut = "";
-            output = tempOut;
+            this.local = local;
+            this.ksis = ksis;
+        }
+
+        public string Execute()
+        {
+            string output = "";
+
+            ArrayList equalSections = new ArrayList();
+            foreach (Section s1 in local.Schedule)
+                foreach (Section s2 in ksis.Schedule)
+                    if (!s1.Equals(s2)) equalSections.Add(s1);
+
+            return output;
         }
     }
 }
