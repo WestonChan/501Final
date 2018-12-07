169a0dfe2c1239db72ac23a963740f8f293ed184
wwchan <wwchan@OBAMA2>
2018-12-06 21:49:08 -0600

    Verify

--- CIS 501 Final Project/Verify.cs
+++ CIS 501 Final Project/Verify.cs
@@ -20,31 +20,10 @@ namespace CIS_501_Final_Project
         {
             string output = "";
 
-            ArrayList ChangedSections = new ArrayList();
+            ArrayList equalSections = new ArrayList();
             foreach (Section s1 in local.Schedule)
                 foreach (Section s2 in ksis.Schedule)
-                    if (!s1.Equals(s2))
-                    {
-                        local.Schedule.Remove(s1);
-                        ksis.Schedule.Remove(s2);
-                    }
-                    else if (s1.Cor.Equals(s2.Cor) && s1.SectionNumber.Equals(s2.SectionNumber))
-                        output += s1.ToString() + " has been changed!";
-
-            foreach (Section s2 in ksis.Schedule)
-                foreach (Section s1 in local.Schedule)
-                    if (!s1.Equals(s2))
-                    {
-                        local.Schedule.Remove(s1);
-                        ksis.Schedule.Remove(s2);
-                    }
-                    else if (s1.Cor.Equals(s2.Cor) && s1.SectionNumber.Equals(s2.SectionNumber))
-                        output += s2.ToString() + " has been changed!";
-
-            foreach (Section s in local.Schedule) output += s.ToString() + " is not found in the KSIS file!";
-            foreach (Section s in ksis.Schedule) output += s.ToString() + " is not found in the Local file!";
-
-            if (output.Equals("")) output = "Verification Success!";
+                    if (!s1.Equals(s2)) equalSections.Add(s1);
 
             return output;
         }
